9c0a72e30c1f90a1add9c27543819b8dd01f7391
Nelson Mwirumubi <mwinel@live.com>
2019-10-28 09:25:29 +0300

    Use AJAX to send data to a flask app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123456@localhost:5432/todos'
@@ -17,11 +18,24 @@ db.create_all()
 
 @app.route("/todos/create", methods=['POST'])
 def create_todo():
-  description = request.form.get('description', '')
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
 
 @app.route("/")
 def index():
