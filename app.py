a615dde940c19f8f3a1ef4b23cc63345d208ae0a
Nelson Mwirumubi <mwinel@live.com>
2019-11-16 09:28:27 +0300

    Add get list of todos

--- app.py
+++ app.py
@@ -77,6 +77,12 @@ def delete_todo(todo_id):
         "success": True
     })
 
+@app.route("/lists/<list_id>")
+def get_list_todos(list_id):
+    return render_template('index.html', 
+        todos=Todo.query.filter_by(list_id=list_id).order_by('id')
+        .all())
+        
 @app.route("/")
 def index():
-    return render_template('index.html', todos=Todo.query.all())
+    return redirect(url_for('get_list_todos', list_id=1))
